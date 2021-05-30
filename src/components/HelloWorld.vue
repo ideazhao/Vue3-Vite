d86763bb22e37163d86899778675b43c03a732e8
ideazhao <420750281@qq.com>
2021-05-30 09:58:59 +0800

    Vue3项目初始化

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>{{ count }} {{ object.foo }}</div>
+</template>
+
+<script>
+import {reactive, ref } from 'vue'
+
+export default {
+  setup() {
+    const count = ref(0)
+    const object = reactive({ foo: 'bar' })
+    
+    setTimeout(() => {
+      // 通过 ref 包裹的变量，需要通过 .value 的形式修改变量，在模板中使用无需使用 .value ，因为在 setup 内 return 的时候，已经帮你接开。
+      count.value = 1
+      object.foo = 'foo'
+    }, 2000)
+
+    // 暴露给模板
+    return {
+      count,
+      object
+    }
+  },
+}
+
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
